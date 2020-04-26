e4abc76d37953594d287099592ce5ef5f19aa603
Joan <joancolmenero@gmail.com>
2020-04-26 20:44:11 +0200

    ♻️ Lints and unused code

--- crypto_list/src/main/java/com/joancolmenerodev/crypto_list/data/retrofit/service/CryptoApi.kt
+++
@@ -1,7 +0,0 @@
-package com.joancolmenerodev.crypto_list.data.retrofit.service
-
-import com.joancolmenerodev.crypto_list.data.model.CryptoResponse
-
-interface CryptoApi {
-    suspend fun getCryptoList() : CryptoResponse
-}
