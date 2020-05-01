cc5bca8066d81324702b12fbf2737f43c07fe842
Joan <joancolmenero@gmail.com>
2020-05-01 12:13:26 +0200

    🔥 Unused methods

--- app/src/androidTest/java/com/joancolmenerodev/showcase_kotlin/base/espresso/UIElement.kt
+++ app/src/androidTest/java/com/joancolmenerodev/showcase_kotlin/base/espresso/UIElement.kt
@@ -5,30 +5,26 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import org.hamcrest.Matcher
 
-class UIElement(matcher : Matcher<View>) {
+class UIElement(matcher: Matcher<View>) {
 
-    private val element : ViewInteraction = onView(matcher)
-
-    fun click(){
-        element.perform(ViewActions.click())
-    }
+    private val element: ViewInteraction = onView(matcher)
 
     fun clickItemAtPosition(position: Int) {
-        element.perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(position, ViewActions.click()))
+        element.perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                position,
+                ViewActions.click()
+            )
+        )
     }
 
     fun isDisplayed() {
         element.check(matches(ViewMatchers.isDisplayed()))
-    }
-
-    fun isNotDisplayed() {
-        element.check(doesNotExist())
     }
 
     fun withText(text: String) {
